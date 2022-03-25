ca557ab76b5a155ffb9a0a0e90075b732e69cc78
ChantiaKindle <chantia.kindle0123@gmail.com>
2022-03-25 15:40:31 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift("Milo");
+}
+function appendCat() {
+    const twocats = [...cats, "Broom"];
+    return twocats;
+}
+
+function prependCat() {
+    const twocats = ["Arnold",...cats];
+    return twocats;
+}
+
+function removeLastCat() {
+    return cats.slice(0, 2);
+    
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
